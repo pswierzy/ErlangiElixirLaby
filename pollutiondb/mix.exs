a1505f84d57eda3042fa48a80d34364fe32abc98
pswierzy <pswierzy@student.agh.edu.pl>
2025-05-12 15:24:20 +0200

    Ecto init

---
+++ pollutiondb/mix.exs
@@ -0,0 +1,29 @@
+defmodule Pollutiondb.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :pollutiondb,
+      version: "0.1.0",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :eex, :wx, :observer, :runtime_tools],
+      mod: {Pollutiondb.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ecto_sql, "~> 3.0"},
+      {:ecto_sqlite3, "~> 0.10"}
+    ]
+  end
+end
